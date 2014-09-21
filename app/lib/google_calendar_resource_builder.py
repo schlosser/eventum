73aef88e2815bdcef3ddcc60d170b7176ad22bfa
Dan Schlosser <dan.r.schlosser@gmail.com>
2014-09-21 12:29:08 -0400

    move to a well written html stripper, rather than my naive regex

--- app/lib/google_calendar_resource_builder.py
+++ app/lib/google_calendar_resource_builder.py
@@ -1,6 +1,6 @@
 from pyrfc3339 import generate
 import pytz
-import re
+from app.lib.text import strip_html
 from app.lib.error import GoogleCalendarAPIError
 
 class GoogleCalendarResourceBuilder():
@@ -13,7 +13,7 @@ class GoogleCalendarResourceBuilder():
         resource = {}
         resource['summary'] = event.title
         resource['location'] = event.location
-        resource['description'] = klass._strip_tags(event.long_description)
+        resource['description'] = strip_html(event.long_description)
         resource['status'] = 'confirmed' if event.published else 'tentative'
         if for_update:
             resource['sequence'] = event.gcal_sequence + 1
@@ -53,11 +53,6 @@ class GoogleCalendarResourceBuilder():
         """"""
         d = s.recurrence_end_date
         return '%d%02d%02dT235959Z' % (d.year, d.month, d.day)
-
-    @classmethod
-    def _strip_tags(klass, html):
-        p = re.compile(r'<.*?>')
-        return p.sub(' ', html)
 
     @classmethod
     def rfc3339(klass, datetime):
--- app/lib/text.py
+++ app/lib/text.py
@@ -76,3 +76,40 @@ def truncate_html(text, truncate_len, truncate_text):
         out += '</%s>' % tag
     # Return string
     return out
+
+
+
+def strip_html(text):
+    """Removes HTML markup from a text string.
+
+    From: http://effbot.org/zone/re-sub.htm#strip-html
+
+    @param text The HTML source.
+    @return The plain text.  If the HTML source contains non-ASCII
+        entities or character references, this is a Unicode string.
+    """
+    def fixup(m):
+        text = m.group(0)
+        if text[:1] == "<":
+            return "" # ignore tags
+        if text[:2] == "&#":
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        elif text[:1] == "&":
+            import htmlentitydefs
+            entity = htmlentitydefs.entitydefs.get(text[1:-1])
+            if entity:
+                if entity[:2] == "&#":
+                    try:
+                        return unichr(int(entity[2:-1]))
+                    except ValueError:
+                        pass
+                else:
+                    return unicode(entity, "iso-8859-1")
+        return text # leave as is
+    return re.sub("(?s)<[^>]*>|&#?\w+;", fixup, text)
