d20c847a984ac14d5c0788427f4fbb966e5394a7
Alan Du <alanhdu@gmail.com>
2014-10-10 16:39:17 -0400

    Log all uncaught exceptions

--- run.py
+++ run.py
@@ -1,13 +1,21 @@
 import logging
 import sys
+import traceback
 
 from app import create_app, run
+
+app = create_app()
+
+@app.errorhandler(Exception)
+def exceptionHandler(error):
+    app.logger.error("Uncaught Exception", exc_info=sys.exc_info()) 
+    app.handle_exception(error) # default error handler
 
 if __name__ == "__main__":
     maxBytes = 1024 * 1024 * 256    # cap files at 256 MB (should we?)
     appHandler = logging.handlers.RotatingFileHandler("log/app.log",
                                                       maxBytes=maxBytes)
-    formatter = logging.Formatter("[%(levelname)s] @ %(asctime)s @ %(filename)s %(funcName)s %(lineno)d: %(message)s")
+    formatter = logging.Formatter("%(levelname)s @ %(asctime)s @ %(filename)s %(funcName)s %(lineno)d: %(message)s")
     appHandler.setLevel(logging.INFO)
     appHandler.setFormatter(formatter)
 
@@ -16,7 +24,6 @@ if __name__ == "__main__":
     accessHandler.setLevel(logging.INFO)
     logging.getLogger("werkzeug").addHandler(accessHandler)
 
-    app = create_app()
     app.logger.addHandler(appHandler)
     app.logger.info("Starting Server")
     run()
