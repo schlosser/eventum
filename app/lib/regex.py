cac6bb69cada6529d5e082857931b14cf3c7b02c
Dan Schlosser <dan.r.schlosser@gmail.com>
2014-11-07 20:51:46 -0500

    fix regex

--- app/lib/regex.py
+++ app/lib/regex.py
@@ -9,5 +9,5 @@ from config.flask_config import ALLOWED_UPLOAD_EXTENSIONS
 
 SLUG_REGEX = r'([0-9]|[a-z]|[A-Z]|\-)+'
 FILENAME_REGEX = r'[\w\-@\|\(\)]+'
-FULL_FILENAME_REGEX = ('(' + FILENAME_REGEX +
+FULL_FILENAME_REGEX = ( FILENAME_REGEX + '(' + 
                        ('|'.join(ALLOWED_UPLOAD_EXTENSIONS)+')'))
