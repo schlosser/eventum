3092e87631977a93048c62e048cbb307f34fe650
Nate Brennand <natebrennand@gmail.com>
2014-11-21 12:16:36 -0500

    fix vagrant installation

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -3,23 +3,26 @@
 
 apt-get -y update
 
-apt-get install -y \
-    git-core \
-    git \
-    mongodb \
-    python \
-    python-pip \
-    python-dev \
-    python-software-properties \
-    vim     # debugging
+apt-get install -y  git-core
+apt-get install -y  git
+apt-get install -y  mongodb
+apt-get install -y  python
+apt-get install -y  python-dev
+apt-get install -y  python-pip
+apt-get install -y  python-software-properties
+apt-get install -y  vim
+apt-get install -y  libncurses5-dev
 
-apt-get update
+apt-get -y update
+
 pip install -r /vagrant/config/requirements.txt
 pip install flake8  # for local testing
 gem install sass
 
 
+mkdir -p /vagrant/log
+
 # TODO: setup database and insert any data that is necessary
-service mongod start
+# service mongodb start
 
 exit 0
