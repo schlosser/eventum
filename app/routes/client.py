98ada39d1b943629ff15ca6e38ecf847b5dad354
Dan Schlosser <dan.r.schlosser@gmail.com>
2014-12-08 02:23:38 -0500

    update labs page with projects

--- app/routes/client.py
+++ app/routes/client.py
@@ -15,7 +15,7 @@ from mongoengine import Q
 client = Blueprint('client', __name__)
 
 _resources = None
-_faqs = None
+_labs_data = None
 _companies = None
 
 @client.route('/', methods=['GET'])
@@ -80,15 +80,15 @@ def labs():
     **Methods:** ``GET``
     """
     force = request.args.get('force') is not None
-    faqs = _get_faqs(force=force)
-    return render_template('labs.html', faqs=faqs)
+    labs_data = _get_labs_data(force=force)
+    return render_template('labs.html', data=labs_data)
 
-def _get_faqs(force=False):
-    global _faqs
-    if not _faqs or force:
-        with open(adi['FAQ_PATH']) as f:
-            _faqs = json.loads(f.read()).get('questions')
-    return _faqs
+def _get_labs_data(force=False):
+    global _labs_data
+    if not _labs_data or force:
+        with open(adi['LABS_DATA_PATH']) as f:
+            _labs_data = json.loads(f.read())
+    return _labs_data
 
 @client.route('/learn', methods=['GET'])
 def learn():
