adba7a670b337eaad1c4dcb1ea6ee17f3182bddf
Alan Du <alanhdu@gmail.com>
2016-09-04 22:00:14 -0700

    Add test_errors.py

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from flask import Flask
+
+from eventum import Eventum
+
+
+@pytest.yield_fixture(scope="session")
+def eventum(tmpdir_factory):
+    app = Flask("testing")
+    app.config["EVENTUM_SETTINGS"] = {
+        "UPLOAD_FOLDER": str(tmpdir_factory.mktemp("upload")),
+        "DELETE_FOLDER": str(tmpdir_factory.mktemp("delete")),
+        "GOOGLE_AUTH_ENABLED": False,
+    }
+    eventum = Eventum(app)
+
+    with eventum.app.app_context():
+        yield eventum
